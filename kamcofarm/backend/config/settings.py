45fbede65148a300e31f62851bffb3db7163a057
Ernest1977 <167555178+Ernest1977@users.noreply.github.com>
2026-08-19 22:03:58 +0200

    Update settings.py and docker-compose.yml

--- kamcofarm/backend/config/settings.py
+++ kamcofarm/backend/config/settings.py
@@ -175,13 +175,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
-        'CONN_MAX_AGE': 60,  # Maintient la connexion active pendant 60 secondes
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': '3306',
+        'CONN_MAX_AGE': 60,  # Assurez-vous que cette ligne se termine par une virgule
     }
 }
 
